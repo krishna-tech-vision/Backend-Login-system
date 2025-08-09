359827ab1060e0260d7ec7aab0632bd51abc6984
krishna-tech-vision <krishnanandamunda120@gmail.com>
2025-08-10 01:55:02 +1000

    Add comment on route

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/users/login", loginRoute);
 
 app.get("/", (req, res) => {
     
-    res.json({ success: true, deployed: true});
+    res.json({ success: true, deployed: true}); // Home route
 });
 
 const port = process.env.PORT || 3000;
